bae4ca6f54ea2dda3216cba7eb0b17fc3e59d516
AbelSantiagoG <abelsantiago1000@gmail.com>
2024-08-25 18:27:15 -0500

    Creación general del header

--- test-class-df/src/components/Header.tsx
+++ test-class-df/src/components/Header.tsx
@@ -3,9 +3,21 @@ import '../styles/Header.css'
 export default function Header(){
     return(
         <header className='header'>
-            <img src="" alt="" />
-            
-            This is the header
+            <a href="#" className="logo">
+                <img src="../src/assets/images/omnitrix.png" alt="Omnitrix logo" />
+                <h2> Ben 10</h2>
+            </a>
+            <nav>
+                <a href="#" className="nav-link">
+                    Inicio
+                </a>
+                <a href="" className="nav-link">
+                    Capítulos
+                </a>
+                <a href="" className="nav-link">
+                    Películas
+                </a>
+            </nav>
         </header>
     )
 }
